8fdb10f46d126c8ca9d0c9c4f42235cb68ad6741
Adam <adammonast@Adams-MBP.attlocal.net>
2019-12-11 21:23:35 -0500

    completed NASA project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
+import InfoCard from "./components/InfoCard";
+import MainCard from "./components/MainCard";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <h1>Nasa Photo of the Day</h1>
+      <InfoCard />
     </div>
   );
 }
 
-export default App;
+export default App
